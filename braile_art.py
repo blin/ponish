c71549bc7fea02d351b5ac369c48ab36539ae09b
Evgenii Petrov <blinunleius@gmail.com>
2025-03-29 14:11:14 +0000

    Add braile_art

---
+++ braile_art.py
@@ -0,0 +1,65 @@
+import argparse
+from pathlib import Path
+from typing import Tuple
+
+from PIL import Image
+
+BRAILLE_DOT_MAP = {
+    (0, 0): 0x01,
+    (0, 1): 0x02,
+    (0, 2): 0x04,
+    (0, 3): 0x40,
+    (1, 0): 0x08,
+    (1, 1): 0x10,
+    (1, 2): 0x20,
+    (1, 3): 0x80,
+}
+RGBPixel = Tuple[int, int, int]
+
+
+def is_dark(pixel: RGBPixel, threshold: float) -> bool:
+    return sum(pixel) < threshold
+
+
+# Translated from
+# https://github.com/TheZoraiz/ascii-image-converter/blob/d05a757c5e02ab23e97b6f6fca4e1fbeb10ab559/image_manipulation/ascii_conversions.go#L176
+def image_to_braille(image_path: str | Path) -> list[str]:
+    img = Image.open(image_path).convert("RGB")
+
+    width, height = img.size
+    target_width = width + (width % 2)
+    target_height = height + (height % 4)
+
+    if (target_width, target_height) != (width, height):
+        img = img.resize((target_width, target_height))
+        width, height = target_width, target_height
+
+    threshold = (255 * 3) / 2
+
+    lines: list[str] = []
+    for y in range(0, height, 4):
+        line = ""
+        for x in range(0, width, 2):
+            braille_code = 0x2800
+            for (dx, dy), dot_value in BRAILLE_DOT_MAP.items():
+                pixel_value = img.getpixel((x + dx, y + dy))
+                assert isinstance(pixel_value, tuple)
+                assert len(pixel_value) == 3
+                if is_dark(pixel_value, threshold):
+                    braille_code += dot_value
+            line += chr(braille_code)
+        lines.append(line)
+    return lines
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Convert image to Braille art.")
+    parser.add_argument("input_path", type=Path, help="Path to the input image file.")
+    args = parser.parse_args()
+    lines = image_to_braille(args.input_path)
+    for line in lines:
+        print(line)
+
+
+if __name__ == "__main__":
+    main()
