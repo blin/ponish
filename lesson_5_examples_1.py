c68a8b7bd0e44c9980062962210714d8e2cd1f69
Evgenii Petrov <blinunleius@gmail.com>
2025-03-31 10:34:45 +0100

    Cleanup first paragraph of part 2 passage 1

--- lesson_5_examples_1.py
+++ lesson_5_examples_1.py
@@ -19,32 +19,30 @@ dir.mkdir(parents=True, exist_ok=True)
 
 texts = [
     (
-        "passage-1",
-        [
-            "Once upon a time , in the magical land of Equestria , there were two regal sisters who ruled",
-            "together and created harmony for all the land . To do this , the eldest used her unicorn powers",
-            "to raise the sun at dawn ; the younger brought out the moon to begin the night . Thus , the two sisters maintained balance for their",
-            "kingdom and their subjects , all the different types of ponies .",
-            # paragraph
-            "But as time went on , the younger sister became resentful . The ponies relished and played in the day",
-            "her elder sister brought forth , but shunned , and slept through her beautiful night . One fateful",
-            "day , the younger unicorn refused to lower the moon to make way for the dawn . The elder sister tried to reason",
-            "with her , but the bitterness in the young ones heart had transformed her into a wicked mare of darkness :",
-            "Nightmare Moon !",
-            # paragraph
-            "She vowed that she would shroud the land in eternal night . Reluctantly , the elder sister harnessed",
-            "the most powerful magic known to Ponydom: the Elements of Harmony . Using the magic of",
-            "the Elements of Harmony , she defeated her younger sister , and banished her permanently in the moon .",
-            "The elder sister took on responsibility for both sun , and moon , and harmony has",
-            "been maintained in Equestria for generations since .",
-        ],
+        # TODO: 2 as a number in a circle
+        "Ons upon a tim n the majikal land of Eqes$(TR)ea $(TH)r wer two regal $(SS)$(TR)s wo ruld",
+        "toge$(TH)er nd $(CR)e$(AT)ed harmony for $(AL) the land . Todo$(TH)is , the eldest usd her unikorn powers",
+        "toraz the sun $(AT)dawn ; the yo$(NG)er $(BR)ot ot the mon tobegin the nit . $(TH)us , the two $(SS)$(TR)s mintind balans for $(TH)r",
+        "ki$(ng)dom nd $(TH)r $(sub)jekts , $(AL) the di$(FR)ent typs of pones .",
+        # paragraph
+        "But as time went on , the younger $(SS)$(TR) became resentful . The ponies relished nd played in the day",
+        "her elder $(SS)$(TR) brought for$(TH) , but shunnd , nd slept $(TH)rough her beautiful nit . One fateful",
+        "day , the younger unikorn refused to lower the moon to make way for the dawn . The elder $(SS)$(TR) $(TR)ied to reason",
+        "wi$(TH) her , but the bitterness in the young ones heart had $(TR)ansformed her into a wicked mare of darkness :",
+        "nitmare Moon !",
+        # paragraph
+        "She vowed $(TH)at she would shroud the land in eternal nit . Reluctantly , the elder $(SS)$(TR) harnessed",
+        "the most powerful magic known to Ponydom: the Elements of Harmony . Using the magic of",
+        "the Elements of Harmony , she defeated her younger $(SS)$(TR) , nd banished her permanently in the moon .",
+        "The elder $(SS)$(TR) took on responsibility for bo$(TH) sun , nd moon , nd harmony has",
+        "been mintind in Eques$(TR)ea for generations since .",
     ),
 ]
 if __name__ == "__main__":
-    for text_name, text in texts:
-        for i, line in list(enumerate(text)):
+    for tid, text in enumerate(texts):
+        for lid, line in list(enumerate(text))[2:6]:
             dc = DrawingContext(
-                output_path=dir / f"{text_name}-line-{i + 1:02}.svg",
+                output_path=dir / f"passage-{tid + 1}-line-{lid + 1:02}.svg",
                 drawing_width=1100,
                 drawing_height=150,
             )
